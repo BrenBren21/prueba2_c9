9003488928cdcf6bb96dc637564d18bf120c7778
BrenBren21 <90345907+BrenBren21@users.noreply.github.com>
2021-11-17 16:53:55 -0600

    Add files via upload

---
+++ PROC9_P5Juego-main/sketch.js
@@ -0,0 +1,20 @@
+var box,box2;
+
+function setup() {
+  createCanvas(600,600);
+}
+
+function draw() 
+{
+  background(30);
+  box=createSprite(300,300,50,50);
+  box2=createSprite(500,500,50,50);
+  
+  
+  drawSprites();
+
+}
+
+
+
+
